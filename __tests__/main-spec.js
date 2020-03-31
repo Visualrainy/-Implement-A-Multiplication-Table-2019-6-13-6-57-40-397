465b7e76d817d018e260a52bbc0c675b5cd3c6dc
Peng Chen <pchen@thoughtworks.com>
2020-03-31 16:09:47 +0800

    Add jest debugger and implements the multiply table

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -4,6 +4,6 @@ it("should return null when the start number is greater than end number", () => 
     expect(multiplyTable(6, 5)).toBe(null)
 })
 
-// it("should print multiply table as expected", () => {
-//     expect(multiplyTable())
-// })
+it("should print multiply table as expected", () => {
+    expect(multiplyTable(5, 6)).toBe('5*5=25\n5*6=30 6*6=36')
+})
--- main.js
+++ main.js
@@ -3,9 +3,20 @@ export default function multiplyTable(start, end) {
         return null;
     }
 
+    let result='';
+
     if(checkNumberRange(start) && checkNumberRange(end)) {
-        
+        for(let right = start; right <= end; right++) {
+            for(let left = start; left <= right; left++) {
+                result += left + '*' + right + '=' + left*right + ' ';
+            }
+            result = result.trim();
+            if(right !== end) {
+                result += '\n';
+            }
+        }
     }    
+    return result;
 }
 
 function compare(start, end) {
